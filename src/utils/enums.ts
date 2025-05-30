8b8f9b7d6fcb1e9fefec23486f14987eabc0424f
rendy278 <kepolaumautauaja@gmail.com>
2025-05-30 20:40:05 +0700

    awad

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,10 +1,14 @@
-export enum ColumnType {
-  TO_DO = "Todo",
-  IN_PROGRESS = "In Progress",
-  BLOCKED = "Blocked",
-  COMPLETED = "Completed",
-}
+export const ColumnType = {
+  TO_DO: "Todo",
+  IN_PROGRESS: "In Progress",
+  BLOCKED: "Blocked",
+  COMPLETED: "Completed",
+} as const;
 
-export enum ItemType {
-  TASK = "Task",
-}
+export type ColumnType = (typeof ColumnType)[keyof typeof ColumnType];
+
+export const ItemType = {
+  TASK: "Task",
+} as const;
+
+export type ItemType = (typeof ItemType)[keyof typeof ItemType];
